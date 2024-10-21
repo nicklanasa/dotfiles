c0eecaccd34d6a223df8f0ec93e770dfade804a1
nytek <nicklanasa@gmail.com>
2024-10-20 20:23:02 -0400

    Initial

---
+++ .config/doom/config.el
@@ -0,0 +1,152 @@
+(setq make-backup-files nil
+      auto-save-default t
+      history-length 25
+      python-indent 4
+      js-indent-level 2
+      typescript-indent-level 2
+      display-line-numbers-type nil
+      global-hl-line-modes t
+      ns-use-proxy-icon nil
+      frame-title-format nil
+      doom-theme 'doom-solarized-dark-high-contrast
+      doom-font (font-spec :family "Consolas" :size 16)
+      gptel-model "llama3.1:8b"
+      explicit-shell-file-name "/bin/zsh"
+      gptel-backend (gptel-make-ollama "Ollama"
+                      :host "localhost:11434"
+                      :stream t
+                      :models '("llama3.1:8b")))
+
+;; set env
+(setenv "PATH" (concat (getenv "PATH") ":/Library/TeX/texbin/"))
+
+;; faces
+(set-face-background 'vertical-border "#073642")
+(set-face-foreground 'vertical-border (face-background 'vertical-border))
+
+;; emacs-mac
+(when (display-graphic-p)
+  (set-frame-parameter (selected-frame) 'alpha '(95 . 95))
+  (add-to-list 'default-frame-alist '(alpha . (95 . 95))))
+
+(after! company-mode
+  (setq company-tooltip-align-annotations t))
+
+(after! dash-docs
+  (set-docsets! 'ts-mode :add "React" "TypeScript"))
+
+;; org-mode
+(after! org
+  (setq org-todo-keywords
+        '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
+          (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)")))
+
+  (setq org-directory
+        (let ((sys (system-name)))
+          (cond
+           ((string-prefix-p "NytekPC" sys) "/mnt/c/Users/Nickl/iCloudDrive/Documents/org/")
+           ((string-prefix-p "Nicks-MacBook-Pro.local" sys) "~/Documents/org/"))))
+
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((shell .t)
+     (emacs-lisp . t)
+     (python . t)
+     (typescript . t)
+     (Javascript . t)))
+
+  (setq org-tag-alist '(("@home" . ?h)
+                        ("@work" . ?w)))
+
+  (setq org-deadline-warning-days 10
+        org-agenda-start-day nil ;; start on current day
+        org-confirm-babel-evaluate nil
+        org-agenda-include-diary t
+        org-agenda-files (directory-files-recursively org-directory "\.org$"))
+
+  (setq org-capture-templates
+        '(("t" "Todo" entry (file (lambda () (expand-file-name "todo.org" org-directory)))
+           "* TODO %?\n  %i\n  %a")
+          ("l" "Ledger transaction" plain
+           (file "~/ledger/2024.ledger")
+           "%(org-read-date) %^{Description}
+ %^{Category|Expenses:Food:Groceries|Expenses:Food:Eating Out|Expenses:Household|Expenses:Subscriptions}
+ %^{Account|Assets:Checking|Income:Cash}  -$%^{Amount}"
+           :empty-lines 1)
+          ("c" "Cookbook" entry (file (lambda () (expand-file-name "cookbook.org" org-directory)))
+           "%(org-chef-get-recipe-from-url)"
+           :empty-lines 1)))
+
+  (setq org-agenda-custom-commands
+        '((" " "Agenda"
+           ((agenda ""
+                    ((org-agenda-span 'day)))
+            (todo "TODO"
+                  ((org-agenda-overriding-header "Unscheduled tasks")
+                   (org-agenda-files (list (expand-file-name "todo.org" org-directory)))
+                   (org-agenda-skip-function '(org-agenda-skip-entry-if 'scheduled 'deadline))
+                   ))
+            (todo "TODO"
+                  ((org-agenda-overriding-header "Unscheduled project tasks")
+                   (org-agenda-files (directory-files-recursively (expand-file-name "projects" org-directory) "\.org$"))
+                   (org-agenda-skip-function '(org-agenda-skip-entry-if 'scheduled 'deadline)))))))))
+
+;; icloud syncing
+(defmacro func-ignore (fnc)
+  "Return function that ignores its arguments and invokes FNC."
+  `(lambda (&rest _rest)
+     (funcall ,fnc)))
+
+;; save all org-buffers when todo state changes
+(advice-add 'org-deadline       :after (func-ignore #'org-save-all-org-buffers))
+(advice-add 'org-schedule       :after (func-ignore #'org-save-all-org-buffers))
+(advice-add 'org-store-log-note :after (func-ignore #'org-save-all-org-buffers))
+(advice-add 'org-todo           :after (func-ignore #'org-save-all-org-buffers))
+
+(after! org-roam
+  (setq org-roam-directory "~/Documents/org/roam"))
+
+;; end org
+
+;; ledger
+(after! ledger
+  (setq ledger-default-date-format "%Y-%m-%d")
+  (setq ledger-reports
+        '(("bud" "%(binary) -f %(ledger-file) reg --budget --monthly ^expenses")
+          ("unbud" "%(binary) -f %(ledger-file) reg --unbudgeted --monthly ^expenses")
+          ("bal" "%(binary) -f %(ledger-file) bal")
+          ("reg" "%(binary) -f %(ledger-file) reg")
+          ("payee" "%(binary) -f %(ledger-file) reg @%(payee)")
+          ("account" "%(binary) -f %(ledger-file) reg %(account)"))))
+
+(after! lsp-ui
+  (setq gc-cons-threshold 100000000
+        read-process-output-max (* 1024 1024)
+        lsp-ui-doc-enable t
+        lsp-ui-doc-position 'bottom
+        lsp-ui-doc-delay 0.1
+        lsp-ui-doc-show-with-cursor t
+        lsp-ui-doc-include-signature nil
+        lsp-ui-sideline-enable nil
+        lsp-ui-sideline-show-hover nil))
+
+;; add to lists
+(add-to-list 'auto-mode-alist '("\\.mdx\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . typescript-tsx-mode))
+(add-to-list 'auto-mode-alist '("\\.restclient\\'" . restclient-mode))
+
+;; global modes
+(global-git-gutter-mode +1)
+(global-auto-revert-mode t)
+(global-visual-line-mode t)
+
+(let ((sys (system-name)))
+  (cond
+   ((string-prefix-p "Nicks-MacBook-Pro.local" sys)
+    (after! doom-ui
+      ;; set your favorite themes
+      (setq custom-safe-themes t)
+      (setq! auto-dark-themes '((doom-solarized-dark-high-contrast) (doom-solarized-light)))
+      (auto-dark-mode))
+    (load "~/dotfiles/.config/doom/personal.el"))))
---
+++ README.md
@@ -0,0 +1,32 @@
+# Setup 
+
+1. link .config from home dir `ln -s dotfiles/.config .config`
+
+2. link tmux config from home dir `ln -s dotfiles/.tmux.conf .tmux.conf`
+
+3. install deps: emacs-plus, tmux, node, git, ripgrep, coreutils, fd fish, tpm, doom-emacs
+
+```
+xcode-select --install
+brew install tmux node git ripgrep coreutils fd fish
+brew tap d12frosted/emacs-plus
+brew install emacs-plus
+ln -s /usr/local/opt/emacs-mac/Emacs.app /Applications/Emacs.app
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+sudo npm i -g typescript-language-server; sudo npm i -g typescript
+git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
+~/.emacs.d/bin/doom install
+```
+
+4. Setup fish and fonts
+
+append fish shell to: /etc/shells, install fisher, tide, monaco nerd fonts by double clicking
+```
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+fisher install IlanCosman/tide@v5
+git clone https://github.com/Karmenzind/monaco-nerd-fonts
+```
+
+# Author 
+
+Nickolas Lanasa
