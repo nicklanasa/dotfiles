f4d76d0b52c4aa49e1e0df67ab0cceecd169022d
nytek <nicklanasa@gmail.com>
2024-10-20 20:23:02 -0400

    Updates

---
+++ .config/doom/init.el
@@ -0,0 +1,60 @@
+(doom! :completion
+       (company +childframe)
+       vertico
+
+       :ui
+       doom
+       doom-dashboard
+       (emoji +unicode)
+       hl-todo
+       modeline
+       ophints
+       (popup +defaults)
+       unicode
+       workspaces
+       zen
+
+       :editor
+       file-templates
+       fold
+       snippets
+       word-wrap
+
+       :emacs
+       dired
+       electric
+       undo
+       vc
+
+       :checkers
+       syntax
+
+       :tools
+       (debugger +lsp)
+       (eval +overlay)
+       (lookup +dictionary +docsets)
+       (lsp +peek)
+       magit
+       make
+       pdf
+       tree-sitter
+
+       :os
+       (:if IS-MAC macos)
+
+       :lang
+       data
+       (emacs-lisp +lsp)
+       (json +lsp)
+       (javascript +lsp)
+       markdown
+       (python +pyenv +lsp)
+       sh
+       ledger
+       (web +lsp)
+       (yaml +lsp)
+       (org +roam2 +dragndrop +pretty)
+
+       :config
+       (default +bindings +smartparens))
+
---
+++ .config/doom/packages.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; -*-
+;;; $DOOMDIR/packages.el
+
+(package! paredit)
+(package! lsp-tailwindcss :recipe (:host github :repo "merrickluo/lsp-tailwindcss"))
+(package! ob-typescript)
+(package! gptel)
+(package! git-gutter)
+(package! restclient)
+(package! org-roam-ui)
