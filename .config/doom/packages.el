8a6edb84e4a8c9c082ddd413ea6481e9f7c3c758
nytek <nicklanasa@gmail.com>
2024-10-20 20:23:02 -0400

    Most recent

---
+++ .config/doom/packages.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; -*-
+;;; $DOOMDIR/packages.el
+
+(package! paredit)
+(package! lsp-tailwindcss :recipe (:host github :repo "merrickluo/lsp-tailwindcss"))
+(package! git-gutter)
+(package! restclient)
+(package! copilot
+  :recipe (:host github :repo "copilot-emacs/copilot.el" :files ("*.el")))
+
+(package! org-chef)
