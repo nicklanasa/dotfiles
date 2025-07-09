d78329983496e51689b51d26e9306ae8a3ac3933
Nickolas Lanasa <nick@mac.mynetworksettings.com>
2025-07-09 18:55:53 -0400

    Updates to org config

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -9,7 +9,7 @@
       global-hl-line-modes nil
       custom-safe-themes t
       doom-theme 'doom-solarized-dark-high-contrast
-      doom-font (font-spec :family "Monaco" :size 15)
+      doom-font (font-spec :family "Cascadia Mono" :size 20)
       explicit-shell-file-name "/bin/zsh")
 
 (set-frame-parameter (selected-frame) 'alpha '(95 . 95))
@@ -27,6 +27,96 @@
         lsp-ui-doc-position 'at-point)
   (add-hook 'lsp-mode-hook 'lsp-ui-doc-mode))
 
+;; org
+(after! org
+  (setq org-src-window-setup 'current-window
+        org-return-follows-link t
+        org-babel-load-languages '((shell .t)
+                                   (emacs-lisp . t)
+                                   (python . t))
+        org-confirm-babel-evaluate nil
+        org-refile-targets '((org-agenda-files :maxlevel . 3))
+        org-agenda-include-diary t
+        org-agenda-tags-column 125
+        org-agenda-start-day nil
+        org-deadline-warning-days 30
+        org-use-speed-commands t)
+
+  (setq org-directory "~/org")
+  (setq org-agenda-files (directory-files org-directory 'full (rx ".org" eos)))
+
+  ;; then add project files
+  (let* ((projects-dir (expand-file-name "projects" org-directory))
+         (project-files (directory-files projects-dir 'full (rx ".org" eos))))
+    (setq org-agenda-files
+          (append org-agenda-files project-files)))
+
+  (setq org-capture-templates
+        '(("t" "Todo" entry (file (lambda () (expand-file-name "inbox.org" org-directory)))
+           "* TODO %?\n  %i\n  %a")
+          ("e" "Event" entry (file (lambda () (expand-file-name "calendar.org" org-directory)))
+           "* %^{Title} %^T\n")
+          ("r" "Weekly Review" entry (file (lambda () (expand-file-name "reviews.org" org-directory)))
+           (file (lambda () (expand-file-name "templates/weeklyreviewtemplate.org" org-directory))))
+          ("c" "Cookbook" entry (file (lambda () (expand-file-name "cookbook.org" org-directory)))
+           "%(org-chef-get-recipe-from-url)"
+           :empty-lines 1)))
+
+  (setq org-agenda-custom-commands
+        '((" " "Agenda"
+           ((agenda ""
+                    ((org-agenda-span 'day)))
+            (todo "TODO"
+                  ((org-agenda-overriding-header "Unscheduled tasks")
+                   (org-agenda-files (list (expand-file-name "inbox.org" org-directory)))
+                   (org-agenda-skip-function
+                    '(org-agenda-skip-entry-if 'scheduled 'deadline))))
+            (todo "TODO"
+                  ((org-agenda-overriding-header "Unscheduled project tasks")
+                   (org-agenda-files
+                    (directory-files (expand-file-name "projects" org-directory)
+                                     'full (rx ".org" eos)))
+                   (org-agenda-skip-function
+                    '(org-agenda-skip-entry-if 'scheduled 'deadline))))))
+          ("w" "Watch Agenda"
+           ((agenda ""
+                    ((org-agenda-span 'day)
+                     (org-agenda-files
+                      (directory-files (expand-file-name "watch" org-directory)
+                                       'full (rx ".org" eos)))))
+            (todo "TODO"
+                  ((org-agenda-overriding-header "Unscheduled watch entries")
+                   (org-agenda-files
+                    (directory-files (expand-file-name "watch" org-directory)
+                                     'full (rx ".org" eos)))
+                   (org-agenda-skip-function
+                    '(org-agenda-skip-entry-if 'scheduled 'deadline))))))
+          ("r" "Read Agenda"
+           ((agenda ""
+                    ((org-agenda-span 'day)
+                     (org-agenda-files
+                      (directory-files (expand-file-name "read" org-directory)
+                                       'full (rx ".org" eos)))))
+            (todo "TODO"
+                  ((org-agenda-overriding-header "Unscheduled read entries")
+                   (org-agenda-files
+                    (directory-files (expand-file-name "read" org-directory)
+                                     'full (rx ".org" eos)))
+                   (org-agenda-skip-function
+                    '(org-agenda-skip-entry-if 'scheduled 'deadline))))))))
+
+  (setq org-todo-keywords
+        '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
+          (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)"))))
+
+(after! org-roam
+  (setq org-roam-directory (file-truename "~/org/roam/"))
+  (setq org-roam-capture-templates '(("d" "default" plain "%?"
+                                      :target
+                                      (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
+                                                 "#+title: ${title}\n")
+                                      :unnarrowed t))))
+
 ;; global modes
 (global-git-gutter-mode +1)
 (global-auto-revert-mode t)
@@ -41,3 +131,8 @@
 (add-to-list 'auto-mode-alist '("\\.tsx\\'" . typescript-tsx-mode))
 (add-to-list 'auto-mode-alist '("\\.restclient\\'" . restclient-mode))
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+;; keyboard shortcuts
+(global-set-key (kbd "<f1>") #'org-capture)
+(global-set-key (kbd "<f2>") #'org-roam-node-find)
+(global-set-key (kbd "<f3>") #'find-file)
