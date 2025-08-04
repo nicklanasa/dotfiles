227a54965d6539516edd10f260d26f37a6903dfa
Nick Lanasa <nicklanasa@gmail.com>
2025-08-04 12:02:21 -0400

    Updates to monitor and packages

--- packages.sh
+++ packages.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo pacman -Sy dbeaver nodejs npm emacs-wayland tmux ledger
+git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/.emacs.d
+sudo pacman -Sy dbeaver nodejs npm emacs-wayland tmux ledger asusctl spotify firefox handbrake vlc xorg-xev mesa-demoszmagit
+yay -Sy 1password sg3_utils rog-control-center nvidia-prime
